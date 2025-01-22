1302d96bee625cb0294a28ff5369c46218622651
MannAmbani1 <Mambani0123@conestogac.on.ca>
2025-01-21 20:43:18 -0500

    Tree algorithms

---
+++ TreeAlgorithms/Algorithms.java
@@ -0,0 +1,42 @@
+package TreeAlgorithms;
+
+public class Algorithms {
+    public static void main(String[] args) {
+        // 5
+        // / \
+        // 2 4
+        // / \ /
+        // 9 10 7
+
+        Node rootNode = new Node();
+        rootNode.data = 5;
+        Node nodeTwo = new Node();
+        nodeTwo.data = 2;
+        Node nodeFour = new Node();
+        nodeFour.data = 4;
+        Node nodeNine = new Node();
+        nodeNine.data = 9;
+        Node nodeTen = new Node();
+        nodeTen.data = 10;
+        Node nodeSeven = new Node();
+        nodeSeven.data = 7;
+
+        rootNode.left = nodeTwo;
+        rootNode.right = nodeFour;
+        nodeTwo.left = nodeNine;
+        nodeTwo.right = nodeTen;
+        nodeFour.left = nodeSeven;
+
+        BinarySearchTree tree = new BinarySearchTree();
+        tree.root = rootNode;
+
+        tree.preOrderTraversal();
+        System.out.println();
+
+        tree.inOrderTraversal();
+        System.out.println();
+
+        tree.postOrderTraversal();
+        System.out.println();
+    }
+}
---
+++ TreeAlgorithms/Node.java
@@ -0,0 +1,7 @@
+package TreeAlgorithms;
+
+public class Node {
+    Node left;
+    Node right;
+    int data;
+}
